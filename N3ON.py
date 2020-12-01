cb9e752f18131c8c3fa2953b07483678f4c7fde7
varsh1408 <71077227+varsh1408@users.noreply.github.com>
2020-12-01 11:04:39 +0530

    Add files via upload

---
+++ N3ON.py
@@ -0,0 +1,33 @@
+import requests
+import sys
+
+def banner():
+  print "HI EVERYONE!"
+  print "FOUNDER: VARSHINI RAMESH & V.SURYA"
+  print "TOOL NAME:N3ON"
+  
+def N3ON(domain):
+    with open("domain.txt","r") as f:
+       for i in f:
+           sub_domain = i.strip()
+           url = sub_domain + "." + domain
+           try:
+               res = requests.get("http://"+url)
+               print("Discovered Subdomain :",url)
+               sys.exit()
+              
+           except:
+                pass
+
+if len(sys.argv) < 2:
+     banner()
+     print("Syntax")
+     print("python N3ON.py Domain_Name")
+     print("Please just enter domain like: google.com not www.google.com")
+     print("Example")
+     print("python N3ON.py google.com")
+else:
+     domain = sys.argv[1]
+     N3ON(domain)
+           
+
